5c3e8774e8e8f95436d35b6208683dabbd70aff4
Amelia Huang <ameliahuang@richitech.com.tw>
2026-04-22 11:16:12 +0800

    移除 coordinate converter router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CoordinateConverter from '@/views/coordinateConverter.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/coordinateConverter',
-      name: 'coordinateConverter',
-      component: CoordinateConverter,
-    },
-  ],
+  routes: [],
 })
 
 export default router
